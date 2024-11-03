cdd37183af63c4538c30ef23dbbb50b39c9c9148
Benjamin Janssens <benji.janssens@gmail.com>
2024-11-03 12:10:40 +0100

    chore: remove unused additional user account

--- modules/host-users.nix
+++ modules/host-users.nix
@@ -13,11 +13,5 @@
   networking.computerName = hostname;
   system.defaults.smb.NetBIOSName = hostname;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users."${username}" = {
-    home = "/Users/${username}";
-    description = username;
-  };
-
   nix.settings.trusted-users = [username];
 }
