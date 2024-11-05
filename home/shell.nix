184b2b4d1c99ee86c721aa74270f933e28702a67
bejanssens <benji.janssens@gmail.com>
2024-11-05 18:00:39 +0100

    fix: use brew shellenv

--- home/shell.nix
+++ home/shell.nix
@@ -3,12 +3,13 @@
     enable = true;
     enableCompletion = true;
     initExtra = ''
-      export PATH="$PATH:/opt/homebrew/bin"
-
       # Node Version Manager
       export NVM_DIR="$HOME/.nvm"
       [ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && \. "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" # This loads nvm
       [ -s "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" ] && \. "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
+    '';
+    profileExtra = ''
+      eval "$(brew shellenv)"
     '';
   };
 
