1d0bbc81562d9504cb8fe156ab176e575e10cce0
bejanssens <benji.janssens@gmail.com>
2024-11-06 14:26:19 +0100

    refactor: move variables out of .zshrc; enable syntax highlighting and autosuggestion in zsh

--- home/shell.nix
+++ home/shell.nix
@@ -2,23 +2,24 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    initExtra = ''
-      export PATH="$PATH:$HOME/go/bin"
+    syntaxHighlighting.enable = true;
+    autosuggestion.enable = true;
 
+    initExtra = ''
       # Node Version Manager
-      export NVM_DIR="$HOME/.nvm"
       [ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && \. "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" # This loads nvm
       [ -s "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" ] && \. "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
-
-      # zcli
-      export AWS_CA_BUNDLE=/opt/homebrew/etc/ca-certificates/cert.pem;
-      export NODE_EXTRA_CA_CERTS=/Users/bejanssens/.zcli/zscaler_root.pem;
     '';
+
     profileExtra = ''
       eval "$(brew shellenv)"
     '';
-  };
 
-  home.shellAliases = {
+    sessionVariables = {
+      PATH = "$PATH:$HOME/go/bin";
+      NVM_DIR = "$HOME/.nvm";
+      AWS_CA_BUNDLE = "/opt/homebrew/etc/ca-certificates/cert.pem";
+      NODE_EXTRA_CA_CERTS = "$HOME/.zcli/zscaler_root.pem";
+    };
   };
 }
