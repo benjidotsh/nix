cfa789b23ecec2305772d4507d322906440cbde3
bejanssens <benji.janssens@gmail.com>
2024-11-07 22:01:48 +0100

    feat: add .hushlogin

--- home/ssh.nix
+++ home/ssh.nix
@@ -6,4 +6,7 @@
         IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
     '';
   };
+
+  # disable the "Last login" message
+  home.file.".hushlogin".text = "";
 }
