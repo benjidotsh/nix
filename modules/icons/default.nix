d89f18322b54ec4f92bc11b09b39c0d726bbb39d
Benjamin Janssens <benji.janssens@gmail.com>
2024-11-28 13:22:40 +0100

    feat: add custom icon for Stardew Valley

--- modules/icons/default.nix
+++ modules/icons/default.nix
@@ -1,10 +1,14 @@
-{...}: {
+{username, ...}: {
   environment.customIcons = {
     enable = true;
     icons = [
       {
         path = "/Applications/Spotify.app";
         icon = ./spotify.icns;
+      }
+      {
+        path = "/Users/${username}/Applications/Stardew Valley.app";
+        icon = ./stardew_valley.icns;
       }
     ];
   };
