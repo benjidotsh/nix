da533c9cb7367809a5c8a90a8d462fdca3bb81cf
Benjamin Janssens <benji.janssens@gmail.com>
2025-02-27 17:04:09 +0100

    fix: combine starship preset and own config instead of overriding it

--- home/starship/default.nix
+++ home/starship/default.nix
@@ -1,11 +1,12 @@
-{...}: {
+{lib, ...}: {
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
 
     settings =
-      builtins.fromTOML (builtins.readFile ./presets/nerd-font-symbols.toml)
-      // {
+      lib.recursiveUpdate
+      (builtins.fromTOML (builtins.readFile ./presets/nerd-font-symbols.toml))
+      {
         docker_context = {
           disabled = true;
         };
