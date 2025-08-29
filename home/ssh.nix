2079f97fbb489647adb237f2ffafc2f94d23b446
Benjamin Janssens <benji.janssens@gmail.com>
2025-08-29 11:14:18 +0200

    chore: update ssh config

--- home/ssh.nix
+++ home/ssh.nix
@@ -1,10 +1,12 @@
 {...}: {
   programs.ssh = {
     enable = true;
-    extraConfig = ''
-      Host *
-        IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
-    '';
+    enableDefaultConfig = false;
+    matchBlocks = {
+      "*" = {
+        identityAgent = "'~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock'";
+      };
+    };
   };
 
   # disable the "Last login" message
